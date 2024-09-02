d17d48e1e00b21d16dad48e4181cb41bfa652fa6
Oppa Yanes <yanes.bahari@gmail.com>
2024-09-02 09:51:19 +0700

    Update area_pohon.sql

--- area_pohon.sql
+++ area_pohon.sql
@@ -1,17 +1,13 @@
 with area_pohon as (
   select 
     ou.name operating_unit, 
-    sum(
-      coalesce(plp.planted_area, 0)
-    ) area, 
-    sum(
-      coalesce(plp.plant_total, 0)
-    ) pohon, 
+    sum(coalesce(plp.planted_area, 0)) area, 
+    sum(coalesce(plp.plant_total, 0)) pohon, 
     case when plp.state = 'immature_plant' then 'TBM' else 'TM' end status 
   from 
     plantation_land_block plb 
-    left join operating_unit ou on ou.id = plb.operating_unit_id 
     left join plantation_land_planted plp on plp.block_id = plb.id 
+    left join operating_unit ou on ou.id = plb.operating_unit_id 
   group by 
     ou.name, 
     status
@@ -20,7 +16,8 @@ select
   ap.operating_unit, 
   ap.area, 
   ap.pohon, 
-  case when ap.area != 0 then ap.pohon / ap.area else ap.pohon / 1 end pohon_per_hektare, 
-  status 
+  ap.pohon / (case when ap.area != 0 then ap.area else 1 end) pohon_per_hektare, 
+  ap.status 
 from 
   area_pohon ap
+;
