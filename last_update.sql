2a45be46a57bc291689bbc4c35d3f976304cadf4
Oppa Yanes <yanes.bahari@gmail.com>
2024-09-03 14:16:17 +0700

    Update last_update.sql

--- last_update.sql
+++ last_update.sql
@@ -1,7 +1,7 @@
-SELECT
-	MAX(datetime_production)::date last_date
-FROM
-	hit_mill_dailyreport_sounding
-WHERE
-	state = 'done'
-;
+SELECT (now()- INTERVAL '1 DAY')::date last_update;
+--SELECT
+--	MAX(datetime_production)::date last_date
+--FROM
+--	hit_mill_dailyreport_sounding
+--WHERE
+--	state = 'done'
